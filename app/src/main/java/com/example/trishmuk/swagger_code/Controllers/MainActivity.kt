77e7397dbaae7bd68fe58a36f84536803efbf9ba
trishank96 <mukherjeetrish005@gmail.com>
2018-06-20 14:16:21 +0530

    Added list view and corresponding layout

--- app/src/main/java/com/example/trishmuk/swagger_code/Controllers/MainActivity.kt
+++ app/src/main/java/com/example/trishmuk/swagger_code/Controllers/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.trishmuk.swagger_code.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.trishmuk.swagger_code.Models.Category
 import com.example.trishmuk.swagger_code.R
+import com.example.trishmuk.swagger_code.Services.DataList
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter : ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_dropdown_item_1line,
+                DataList.categories)
+        categoryListview.adapter = adapter
     }
 }
