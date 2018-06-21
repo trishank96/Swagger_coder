c2b15408a83bec55af7fb1328f3adf87daa10886
trishank96 <mukherjeetrish005@gmail.com>
2018-06-21 12:43:24 +0530

    Added a custom listview adapter

--- app/src/main/java/com/example/trishmuk/swagger_code/Controllers/MainActivity.kt
+++ app/src/main/java/com/example/trishmuk/swagger_code/Controllers/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.trishmuk.swagger_code.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import com.example.trishmuk.swagger_code.Adapters.CategoryAdapter
 import com.example.trishmuk.swagger_code.Models.Category
 import com.example.trishmuk.swagger_code.R
 import com.example.trishmuk.swagger_code.Services.DataList
@@ -11,12 +11,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_dropdown_item_1line,
+        adapter = CategoryAdapter(this,
                 DataList.categories)
         categoryListview.adapter = adapter
     }
