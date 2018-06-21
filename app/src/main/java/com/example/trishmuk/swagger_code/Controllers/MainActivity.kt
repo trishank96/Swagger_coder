c7ec45d96e35d42f4fa78fcd183dafafb37309bc
trishank96 <mukherjeetrish005@gmail.com>
2018-06-21 14:36:54 +0530

    Added Recycle view and adapter

--- app/src/main/java/com/example/trishmuk/swagger_code/Controllers/MainActivity.kt
+++ app/src/main/java/com/example/trishmuk/swagger_code/Controllers/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.trishmuk.swagger_code.Controllers
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import com.example.trishmuk.swagger_code.Adapters.CategoryAdapter
+import com.example.trishmuk.swagger_code.Adapters.RecycleViewAdapter
 import com.example.trishmuk.swagger_code.Models.Category
 import com.example.trishmuk.swagger_code.R
 import com.example.trishmuk.swagger_code.Services.DataList
@@ -11,12 +13,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : RecycleViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = CategoryAdapter(this,
-                DataList.categories)
+        adapter = RecycleViewAdapter(this, DataList.categories)
         categoryListview.adapter = adapter
+
+        val layoutMan = LinearLayoutManager(this)
+        categoryListview.layoutManager = layoutMan
+        categoryListview.setHasFixedSize(true)
     }
 }
