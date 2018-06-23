3b236b602737a0e754972b1559f34585b71af9ff
trishank96 <mukherjeetrish005@gmail.com>
2018-06-23 13:38:07 +0530

    Product activity finished

---
+++ app/src/main/java/com/example/trishmuk/swagger_code/Controllers/ItemActivity.kt
@@ -0,0 +1,13 @@
+package com.example.trishmuk.swagger_code.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trishmuk.swagger_code.R
+
+class ItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+    }
+}
--- app/src/main/java/com/example/trishmuk/swagger_code/Models/Product.kt
+++ app/src/main/java/com/example/trishmuk/swagger_code/Models/Product.kt
@@ -1,3 +1,7 @@
 package com.example.trishmuk.swagger_code.Models
 
-class Product (val title: String, val price: String, val image :String)
+class Product constructor(val title: String, val price: String, val image :String){
+    override fun toString(): String {
+        return title
+    }
+}
